c28d3e9e8bde73203ab7e8b3e2cc13bd46e24e6e
qwertytr1 <makhnach20040208@gmail.com>
2025-01-02 23:22:50 +0300

    ff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
+import { useContext, useEffect } from 'react';
 import Login from './components/login/login';
 import ProfilePage from './components/profile/profile';
 import Register from './components/register/register';
@@ -10,15 +11,22 @@ import TemplateDetailsPage from './components/template/templateDetailsPage';
 import TemplatesPage from './components/template/templatesView/templatePage';
 import CreateTemplateModal from './components/template/addTemplate/addTemplateParts/addtemplate';
 import TestPage from './components/testPage/testPage';
+import Context from '.';
 
 function App() {
+  const navigate = useNavigate();
+  const { store } = useContext(Context);
+  useEffect(() => {
+    if (!store.checkAuth()) {
+      navigate('/login');
+    }
+  }, [navigate, store]);
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/test/:id" element={<TestPage />} />
-      <Route path="/" element={<Login />} />
-      <Route path="/menu" element={<SidebarMenu />}>
+      <Route path="/" element={<SidebarMenu />}>
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/admin" element={<AdminPanel />} />
         <Route path="/templates/create" element={<CreateTemplateModal />} />
