aae29011dff4b285eae660538d54edf346dbe50d
Ilya <105173209+qwertytr1@users.noreply.github.com>
2025-01-08 23:42:04 +0300

    Update database.js

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -2,15 +2,17 @@ const Sequelize = require('sequelize');
 const mysql2 = require('mysql2');
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    logging: false,
-  }
-);
+const sequelize = new Sequelize({
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  dialect: 'mysql',
+  logging: false,
+  pool: {
+    acquire: 30000,
+    idle: 10000,
+  },
+});
 
 module.exports = sequelize;
