c34e65a1c43589a62dc3df424c259e031f183144
qwertytr1 <makhnach20040208@gmail.com>
2025-01-16 13:36:02 +0300

    add integration

--- server/api/app.js
+++ server/api/app.js
@@ -1,39 +1,3 @@
-// require("dotenv").config();
-// const express = require("express");
-// const sequelize = require("../src/config/database");
-// const apiRoutes = require("../src/routes/api");
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const errorMiddleware = require('../src/middleware/error-middleware.js');
-// const app = express();
-
-// app.use(cors({
-//   origin: 'https://final-project-4v8o.vercel.app',
-// //  origin: 'http://localhost:3001',
-//   methods: ['GET', 'POST', 'PUT','PATCH', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true
-// }));
-
-// app.use(bodyParser.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.use("/api", apiRoutes);
-
-// app.use(errorMiddleware);
-
-// console.log("Starting the server...");
-// export default (req, res) => {
-//   sequelize
-//     .sync({ force: false })
-//     .then(() => {
-//       app(req, res);
-//     })
-//     .catch((err) => {
-//       console.error("Failed to sync database:", err);
-//       res.status(500).json({ error: 'Failed to sync database' });
-//     });
-// }
 require("dotenv").config();
 const express = require("express");
 const sequelize = require("../src/config/database");
@@ -44,7 +8,7 @@ const errorMiddleware = require('../src/middleware/error-middleware.js');
 const app = express();
 
 app.use(cors({
-  origin: 'https://final-project-31fn.vercel.app',
+  origin: 'https://final-project-4v8o.vercel.app',
 //  origin: 'http://localhost:3001',
   methods: ['GET', 'POST', 'PUT','PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
@@ -58,11 +22,15 @@ app.use("/api", apiRoutes);
 
 app.use(errorMiddleware);
 
-sequelize
-  .sync({ force: false })
-  .then(() => {
-    app.listen(process.env.PORT || 3000, () => {
-      console.log("Server running on port 3000");
+console.log("Starting the server...");
+export default (req, res) => {
+  sequelize
+    .sync({ force: false })
+    .then(() => {
+      app(req, res);
+    })
+    .catch((err) => {
+      console.error("Failed to sync database:", err);
+      res.status(500).json({ error: 'Failed to sync database' });
     });
-  })
-  .catch((err) => console.error("Failed to sync database:", err));
+}
